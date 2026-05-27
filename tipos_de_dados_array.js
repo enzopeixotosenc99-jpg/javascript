f3a1b7db119b6a8c7108fd30199e1754597dc490
enzo peixoto <enzopeixotosenc99@gmail.com>
2026-05-28 10:28:56 +1400

    adicionado outros dados

---
+++ tipos_de_dados_array.js
@@ -0,0 +1,8 @@
+let frutas = ["maçã", "banana", "laranja"];
+let numeros = [1, 2, 3, 4, 5];
+let misturado = ["texto", 42, true, null, undefined, { nome: "objeto" }, [1, 2, 3]];
+
+console.log(frutas);
+console.log(frutas[0]); // Imprime: maçã
+console.log(numeros);
+console.log(misturado);
---
+++ tipos_de_dados_object.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: "carlos",
+    idade: 28,
+    casado: "true"
+};
+console.log(pessoa); // Imprime: { nome: 'carlos', idade: 28, casado: 'true' }
+console.log(pessoa.nome); // Imprime: carlos
+console.log(pessoa.idade);
